1bf7879437d2654f35aeda30d6513286cc415639
Jv650 <141464576+Jv650@users.noreply.github.com>
2024-11-17 21:38:34 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Joke from "./components/Joke";
+//import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joke Generator using React and Joke API</h1>
+      <Joke />
+    </div>
+  );
+}
+
+export default App;
